74f56694c289844664bed5733d170e9d04c82c1b
Documenter.jl <documenter@juliadocs.github.io>
2021-12-21 00:09:16 +0000

    build based on 5376708

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MeshBasisFunctions","category":"page"},{"location":"#MeshBasisFunctions","page":"Home","title":"MeshBasisFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MeshBasisFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MeshBasisFunctions]","category":"page"}]
+}
